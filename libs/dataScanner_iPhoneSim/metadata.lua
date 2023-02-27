50cbc1458f090fc01d949c3d6b56bd2e9e698a9f
yousaf-shah <59312216+yousaf-shah@users.noreply.github.com>
2023-02-27 23:26:59 +0000

    Build for iPhoneSim Restored Product Folder

---
+++ libs/dataScanner_iPhoneSim/metadata.lua
@@ -0,0 +1,13 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'plugin_dataScanner', },
+		frameworks = {"VisionKit"},
+		frameworksOptional = {},
+		usesSwift = true,
+	},
+}
+
+return metadata
